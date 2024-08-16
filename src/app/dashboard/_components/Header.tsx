f0cfb3c4ecb55126afd7a2ecd4b7e01dba64de75
Muhammad Ibad <ibad3572@gmail.com>
2024-08-16 22:02:47 +0500

    Customized Header

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -1,8 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { Search } from 'lucide-react'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='p-5 shadow-sm border-b-2 flex justify-between items-center'>
+      <div className="flex items-center rounded-md gap-2 p-2 border max-w-xl">
+        <Search/>
+        <input type="text" placeholder="Search" className="outline-none"/>
+      </div>
+      <div className="pl-2">
+       <Button
+       className='rounded-2xl text-sm h-8'
+       >🔥 Join Membership for $9.99/Month</Button>
+      </div>
+    </div>
   )
 }
 
