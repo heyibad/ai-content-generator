95dbdfdd938dd45687535c1e594406d1f6316341
Muhammad Ibad <ibad3572@gmail.com>
2024-08-16 22:53:01 +0500

    refactor: Update Header component styling and add search functionality

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -4,16 +4,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='p-5 shadow-sm border-b-2 flex justify-between items-center'>
-      <div className="flex items-center rounded-md gap-2 p-2 border max-w-xl">
-        <Search/>
+    <div className='p-2 py-5 md:p-5  shadow-sm border-b-2 flex justify-between items-center gap-4'>
+      <div className="flex items-center rounded-3xl gap-8 py-2 px-6 border max-w-xl">
+        <Search className='w-4'/>
         <input type="text" placeholder="Search" className="outline-none"/>
       </div>
       <div className="pl-2">
-       <Button
-       className='rounded-2xl text-sm h-8'
-       >🔥 Join Membership for $9.99/Month</Button>
-      </div>
+      <Button
+  className='rounded-3xl text-sm h-10'>
+  <span className="inline">🔥Join Membership</span>
+  <span className="hidden md:inline"> for $9.99/Month</span>
+</Button>
+
+      </div>        
     </div>
   )
 }
--- src/app/dashboard/_components/SideNav.tsx
+++ src/app/dashboard/_components/SideNav.tsx
@@ -1,7 +1,9 @@
+"use client"
 import { FileClock, Home, Settings, WalletCards } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
 
 function SideNav() {
     const MenuList = [
@@ -26,20 +28,28 @@ function SideNav() {
             path: "/dashboard/setting",
         },
     ];
+    const path = usePathname();
+
+    useEffect(() => {
+        console.log(path);
+    }, [path]);
     return (
         <div className="h-screen p-5 shadow-sm border">
             <div className="flex justify-center">
                 <Image src={"/logo.svg"} alt="logo" width={100} height={100} />
             </div>
-            <div className="mt-10">
+            <div className="mt-16">
                 {MenuList.map((menu, index) => {
                     return (
-                        <div key={index} className="flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white hover:rounded-lg cursor-pointer">
-                            <menu.icon />
+                        <div
+                            key={index}
+                            className={`flex gap-3 mb-2 p-2 pl-12 text-xl items-center hover:bg-primary hover:text-white hover:rounded-lg cursor-pointer
+                    ${path == menu.path && "bg-primary text-white rounded-lg"}        
+                            `}
+                        >
+                            <menu.icon className="h-5 w-5" />
                             <h2>
-                                <Link href={menu.path}>
-                                  {menu.name}
-                                  </Link>
+                                <Link href={menu.path}>{menu.name}</Link>
                             </h2>
                         </div>
                     );
