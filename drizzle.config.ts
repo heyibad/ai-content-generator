8cfef5113a06ce3c5f7c75d73d6953126c6471ca
Muhammad Ibad <ibad3572@gmail.com>
2024-09-20 17:00:25 +0500

    Refactor database configuration to use environment variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
     schema:"./utils/schema.ts",
     dialect:"postgresql",
     dbCredentials:{
-        url:"postgresql://ai-content-sass_owner:DaxHw6QnOv9k@ep-restless-sunset-a1aw2rd8.ap-southeast-1.aws.neon.tech/ai-content-sass?sslmode=require"
+        url:String(process.env.NEXT_PUBLIC_NEON_DB!)
     }})
