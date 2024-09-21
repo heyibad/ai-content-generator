4a42649c885727871bde0a8113adbe9f4971d447
Muhammad Ibad <ibad3572@gmail.com>
2024-09-21 14:52:47 +0500

    Refactor Logout component to add logout functionality and improve UI

---
+++ src/app/dashboard/_components/Logout.tsx
@@ -0,0 +1,19 @@
+
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import { signOut } from "next-auth/react";
+import React from "react";
+
+const Logout = () => {
+
+    return (
+        <Button className="gap-1 hover:bg-red-500 h-9"
+        onClick={() => signOut()}
+        >
+            <LogOut size={16} />
+            Logout
+        </Button>
+    );
+};
+
+export default Logout;
