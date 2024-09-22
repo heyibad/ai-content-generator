2fa9a6087044cae25d5f0fed171351b21aeee573
Muhammad Ibad <ibad3572@gmail.com>
2024-09-23 01:18:15 +0500

    Fixed DB issues

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,8 @@
 import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
     schema:"./utils/schema.ts",
+    out: "./utils/migrations",
     dialect:"postgresql",
     dbCredentials:{
-        url:String(process.env.NEXT_PUBLIC_NEON_DB!)
+        url:String(process.env.NEON_DB_URL!)
     }})
--- utils/db.ts
+++ utils/db.ts
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-console.log(process.env.NEXT_PUBLIC_NEON_DB)
-const sql = neon(process.env.NEXT_PUBLIC_NEON_DB!);
+
+const sql = neon(process.env.NEON_DB_URL!);
 export const db = drizzle(sql,{schema});
