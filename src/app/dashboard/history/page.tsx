ac9bcfaddfe2f667477adc8456c4406395c5ebce
Muhammad Ibad <ibad3572@gmail.com>
2024-09-23 19:21:21 +0500

    Added History page to dashboard

---
+++ src/app/dashboard/history/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HistoryTable from '../_components/HistoryTable'
+
+const Page = () => {
+  return (
+    <div>
+                <HistoryTable/>
+    </div>
+  )
+}
+
+export default Page
