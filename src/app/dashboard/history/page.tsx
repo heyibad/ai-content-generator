c9a3d77a4c15f92a4cd7ef9c0fa04478358e4bc6
Muhammad Ibad <ibad3572@gmail.com>
2024-10-05 23:00:36 +0500

    Refactor history page to improve UI and add search functionality

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import HistoryTable from '../_components/HistoryTable'
+import React from "react";
+import HistoryTable from "../_components/HistoryTable";
 
 const Page = () => {
-  return (
-    <div>
-                <HistoryTable/>
-    </div>
-  )
-}
+    return (
+        <div className="bg-white m-5 p-4 dark:bg-[#18181B] dark:text-white rounded h-screen">
+            <div className="py-2">
+                {" "}
+                <h1 className="text-3xl font-extrabold">History</h1>
+                <p className="py-1 text-gray-600">Search your all previous ai generated content</p>{" "}
+            </div>
+            <div>
+              <HistoryTable />  
+            </div>
+          
+        </div>
+    );
+};
 
-export default Page
+export default Page;
