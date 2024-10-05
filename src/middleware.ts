7c794bcbdb99f1dbfb391ad19bad65b8e4e60137
Muhammad Ibad <ibad3572@gmail.com>
2024-10-05 22:59:53 +0500

    Refactor middleware to handle authentication and redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,40 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/login', '/signup', '/', '/verify/:path*'],
+    matcher: ["/dashboard/:path*", "/login", "/signup", "/", "/verify/:path*"],
 };
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request, 
-    secret: process.env.JWT_SECRET,
-    secureCookie: process.env.NODE_ENV === "production"
-  });
-  console.log("Token in middleware:", token);
-  
-  const url = request.nextUrl;
+    const token = await getToken({
+        req: request,
+        secret: process.env.JWT_SECRET,
+        secureCookie: process.env.NODE_ENV === "production",
+    });
+    console.log("Token in middleware:", token);
 
-  if (
-    token &&
-    (url.pathname.startsWith('/login') ||
-      url.pathname.startsWith('/signup') ||
-      url.pathname.startsWith('/verify') ||
-      url.pathname === '/')
-  ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
+    const url = request.nextUrl;
 
-  if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
+    if (
+        token &&
+        (url.pathname.startsWith("/login") ||
+            url.pathname.startsWith("/signup") ||
+            url.pathname.startsWith("/verify") ||
+            url.pathname === "/")
+    ) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
 
-  return NextResponse.next();
- }
+    if (!token && url.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+    if (token?.isVerified && url.pathname.startsWith("/verify")) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+    if (!token?.isVerified && url.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/verify", request.url));
+    }
+
+    return NextResponse.next();
+}
