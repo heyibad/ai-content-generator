dd5814e444caee793b7b9ae321a1e4d8a4f68220
Muhammad Ibad <ibad3572@gmail.com>
2024-10-22 23:17:41 +0500

    Refactor Usage component to display total usage and add upgrade button

--- src/app/dashboard/_components/Usage.tsx
+++ src/app/dashboard/_components/Usage.tsx
@@ -1,36 +1,35 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {  useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 import React, { useContext, useEffect, useState } from "react";
 import { db } from "../../../../utils/db";
 import { Content } from "../../../../utils/schema";
 import { eq } from "drizzle-orm";
 import { TotalUsage } from "@/app/(context)/TotalUsageContext";
+import Link from "next/link";
 
 function Usage() {
     const { data: session, status } = useSession();
 
     const user = session?.user?.email;
-    const {totalUsage,setTotalUsage}=useContext(TotalUsage)
+    const { totalUsage, setTotalUsage } = useContext(TotalUsage);
     async function getData() {
         const results = await db
             .select()
             .from(Content)
-            .where(
-                eq(Content.createdBy, user!)
-            );
-            usage(results);
+            .where(eq(Content.createdBy, user!));
+        usage(results);
     }
-    const usage =  (result:any) => {
+    const usage = (result: any) => {
         let total: number = 0;
-        result.forEach((element:any) => {
+        result.forEach((element: any) => {
             total += Number(element.aiResponse?.length);
         });
-        setTotalUsage(total)
+        setTotalUsage(total);
     };
     useEffect(() => {
-        user&& getData();
+        user && getData();
     }, [user]);
     return (
         <div className="m-5">
@@ -40,18 +39,22 @@ function Usage() {
                     <div
                         className="h-2 bg-white rounded-full"
                         style={{
-                            width: `${(totalUsage/10000)*100}%`,
+                            width: `${(totalUsage / 10000) * 100}%`,
                         }}
                     ></div>
                 </div>
-                <div className="text-sm my-2">{totalUsage}/10,000 Credits Usage</div>
+                <div className="text-sm my-2">
+                    {totalUsage}/10,000 Credits Usage
+                </div>
             </div>
-            <Button
-                variant="secondary"
-                className="text-primary font-semibold w-full my-3"
-            >
-                Upgrade
-            </Button>
+            <Link href="/dashboard/billing">
+                <Button
+                    variant="secondary"
+                    className="text-primary font-semibold w-full my-3"
+                >
+                    Upgrade
+                </Button>
+            </Link>
         </div>
     );
 }
