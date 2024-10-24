ce14af8e0789b62dfe0b2f047f75e880f6c2f865
Muhammad Ibad <ibad3572@gmail.com>
2024-10-24 17:37:14 +0500

    Adjust height of OutputSection component based on screen size

--- src/app/dashboard/tool/_components/OutputSection.tsx
+++ src/app/dashboard/tool/_components/OutputSection.tsx
@@ -19,7 +19,7 @@ function OutputSection({ output }: { output: string }) {
   }, [output]);
 
   return (
-    <div className={`py-4 shadow-md border rounded-lg bg-white dark:bg-[#18181B] col-span-2 h-[100vh] w-[85vw] md:w-full`}>
+    <div className={`py-4 shadow-md border rounded-lg bg-white dark:bg-[#18181B] col-span-2 h-[70vh] mt-20 mt:mt-auto  md:[h-10vh] w-[85vw] md:w-full`}>
       <div className="flex justify-between items-center pb-3 px-3">
         <h2 className="font-bold m-1 text-2xl text-primary">Output</h2>
         <Button
